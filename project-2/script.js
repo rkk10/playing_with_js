1c8ffd42ec8b5b4b7c41d1e5f94b8d705ed52868
Ravindra K Khandagale <ravindrakk143@gmail.com>
2016-07-04 11:54:14 +0530

    click event ,,hide and show

---
+++ project-2/script.js
@@ -0,0 +1,16 @@
+var showButton =
+    document.querySelector('button:first-child');
+
+var hideButton =
+    document.querySelector('button:nth-child(2)');    
+    
+var boxwala = document.querySelector('.box');
+    boxwala.style.borderRadius = '50%';
+    
+    showButton.addEventListener('click', () =>{
+        boxwala.classList.add('show');
+    }); 
+    
+    hideButton.addEventListener('click', () =>{
+        boxwala.classList.remove('show');
+    });   
