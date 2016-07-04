eb376c012e4a7c63b844503b47eff1089a3e266d
Ravindra K Khandagale <ravindrakk143@gmail.com>
2016-07-04 11:55:54 +0530

    simple js example

---
+++ employee.js
@@ -0,0 +1,9 @@
+const EMP_INSU = 500
+
+var employee = { 
+    emp_name : (prompt("Employee Name")),
+    emp_post : (prompt("Employee Post")),
+    emp_salary : (prompt("Employee Salary")),
+    
+    
+};
