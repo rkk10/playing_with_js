07f89277ee7832b46b03fdb82cc0eaa8350292ce
Ravindra K Khandagale <ravindrakk143@gmail.com>
2016-07-16 13:28:16 +0530

    project-3 script and css

---
+++ project-3/script.js
@@ -0,0 +1,23 @@
+/* left menu */
+
+var left_menu = document.querySelector('#menu_btn');
+var close = document.querySelector('#close_btn');
+
+var nav = document.querySelector('.nav_container');
+left_menu.addEventListener('click', () => {
+    // if (left_menu.classList.contains == 'menu')
+    // return;
+    
+    nav.style.translate = '0px';
+    
+    
+});
+
+close.addEventListener('click', () => {
+    // if (left_menu.classList.contains == 'menu')
+    // return;
+    
+    nav.style.translate = '-350px';
+    
+    
+});
